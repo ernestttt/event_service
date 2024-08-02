0b726a8be9c9d41dc12ecf50b922b7aae182ca49
Errnest <yuri.golubovski091@gmail.com>
2024-08-02 12:17:52 +0300

    initial commit

---
+++ Assets/EventService.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EventService : MonoBehaviour
+{
+    [SerializeField] private string serverUrl;
+    
+    public void TrackEvent(string type, string data){
+
+    }
+}
